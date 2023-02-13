198fb9875c0dfd22aa4adcb208ab66e637f04bf0
PG\Infor <c14210098@john.petra.ac.id>
2023-02-13 16:16:38 +0700

    grafkom4

--- src/main/java/Engine/Object2d.java
+++ src/main/java/Engine/Object2d.java
@@ -4,44 +4,46 @@ import org.joml.Vector3f;
 
 import java.util.List;
 
-import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengles.GLES20.glEnableVertexAttribArray;
-import static org.lwjgl.opengles.GLES20.glVertexAttribPointer;
-import static org.lwjgl.opengles.GLES30.glBindVertexArray;
-import static org.lwjgl.opengles.GLES30.glGenVertexArrays;
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL30.*;
 
-public class Object2d extends ShaderProgram {
+public class Object2d extends ShaderProgram{
     List<Vector3f> vertices;
-
     int vao;
     int vbo;
-    public Object2d(List<ShaderModuleData>shaderModuleDataList,List<Vector3f>vertices){
+    public Object2d(List<ShaderModuleData> shaderModuleDataList ,List<Vector3f> vertices) {
         super(shaderModuleDataList);
-        this.vertices=vertices;
+        this.vertices = vertices;
         setupVAOVBO();
-
     }
+
     public void setupVAOVBO(){
-        //set vao
         vao = glGenVertexArrays();
         glBindVertexArray(vao);
 
-
-        //set vbo
         vbo = glGenBuffers();
         glBindBuffer(GL_ARRAY_BUFFER,vbo);
+
         glBufferData(GL_ARRAY_BUFFER,Utils.listoFloat(vertices),GL_STATIC_DRAW);
+
+
+
     }
+
     public void drawSetup(){
         bind();
-        //Bind VBO
+
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER,vbo);
         glVertexAttribPointer(0,3,GL_FLOAT,false,0,0);
     }
+
     public void draw(){
         drawSetup();
-        //Draw the vertices
+
         glLineWidth(1);
         glPointSize(0);
         glDrawArrays(GL_TRIANGLES,0,vertices.size());
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import Engine.Object2d;
 import Engine.ShaderProgram;
 import Engine.Window;
 import org.joml.Vector3f;
-import org.joml.Vector4f;
 import org.lwjgl.opengl.GL;
 
 import java.util.ArrayList;
@@ -14,68 +13,48 @@ import static org.lwjgl.opengl.GL11.glClearColor;
 import static org.lwjgl.opengl.GL30.*;
 
 public class Main {
-    private Window window =
-            new Window
-                    (800,800,"Hello World");
-    private ArrayList<Object2d> objects
-            = new ArrayList<>();
+
+    private Window window = new Window(800,800,"Hello world");
+
+    ArrayList<Object2d> objects = new ArrayList<>();
+
+
+
+    public void loop(){
+        while (window.isOpen()){
+            window.update();
+            glClearColor(0.0f,0.0f,0.0f,0.0f);
+            GL.createCapabilities();
+            //Code ...
+            for (Object2d object:objects){
+                object.draw();
+            }
+
+            glDisableVertexAttribArray(0);
+
+            glfwPollEvents();
+        }
+    }
+
     public void init(){
         window.init();
         GL.createCapabilities();
 
-        //code
-        objects.add(new Object2d(
-                Arrays.asList(
-                        //shaderFile lokasi menyesuaikan objectnya
-                        new ShaderProgram.ShaderModuleData
-                                ("resources/shaders/scene.vert"
-                                        , GL_VERTEX_SHADER),
-                        new ShaderProgram.ShaderModuleData
-                                ("resources/shaders/scene.frag"
-                                        , GL_FRAGMENT_SHADER)
-                ),
-                new ArrayList<>(
-                        List.of(
-                                new Vector3f(0.0f,0.5f,0.0f),
-                                new Vector3f(-0.5f,-0.5f,0.0f),
-                                new Vector3f(0.5f,-0.5f,0.0f)
-                        )
-                )
-        ));
+        objects.add(new Object2d(Arrays.asList(new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert",GL_VERTEX_SHADER),new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag",GL_FRAGMENT_SHADER)),
+                new ArrayList<>(List.of(
+                        new Vector3f(0.0f,0.5f,0.0f),
+                        new  Vector3f(-0.5f,-0.5f,0.0f),
+                        new Vector3f(0.5f,-0.5f,0.0f)))));
     }
-    public void loop(){
-        while (window.isOpen()) {
-            window.update();
-            glClearColor(0.0f,
-                    0.0f, 0.0f,
-                    0.0f);
-            GL.createCapabilities();
 
-            //code
-            for(Object2d object: objects){
-                object.draw();
-            }
-
-
-            // Restore state
-            glDisableVertexAttribArray(0);
-
-            // Poll for window events.
-            // The key callback above will only be
-            // invoked during this call.
-            glfwPollEvents();
-        }
-    }
-    public void run() {
-
+    public void run(){
         init();
         loop();
 
-        // Terminate GLFW and
-        // free the error callback
         glfwTerminate();
         glfwSetErrorCallback(null).free();
     }
+
     public static void main(String[] args) {
         new Main().run();
     }
